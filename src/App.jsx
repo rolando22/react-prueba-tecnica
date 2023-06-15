e2cb8087afdae6679b375678bc8ba7b29e8196cc
rolando22 <rolando2207@gmail.com>
2023-06-15 20:37:31 -0300

    [ADD] petición del hecho aleatorio

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
+
 export function App() {
+    const [fact, setFact] = useState('');
+
+    useEffect(() => {
+        const getRandomFact = async () => {
+            try {
+                const response = await fetch(CAT_ENDPOINT_RANDOM_FACT);
+                const json = await response.json();
+                const { fact } = json;
+                setFact(fact);
+                return fact
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        getRandomFact();
+    }, []);
+
     return (
-        <h1>Hola Mundo</h1>
+        <main>
+            <h1>App de gatitos</h1>
+            <p>{fact}</p>
+        </main>
     );
 }
