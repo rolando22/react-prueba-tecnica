28a521c5eaceeb8c79962db6e5c0100c317097e9
rolando22 <rolando2207@gmail.com>
2023-06-15 21:06:21 -0300

    [ADD] petición imágen aleatoria

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 
+import './App.css';
+
+const CAT_ENDPOINT = 'https://cataas.com';
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
 
 export function App() {
     const [fact, setFact] = useState('');
+    const [imgUrl, setImgUrl] = useState('');
 
     useEffect(() => {
         const getRandomFact = async () => {
@@ -12,7 +16,6 @@ export function App() {
                 const json = await response.json();
                 const { fact } = json;
                 setFact(fact);
-                return fact
             } catch (error) {
                 console.log(error);
             }
@@ -21,10 +24,32 @@ export function App() {
         getRandomFact();
     }, []);
 
+    useEffect(() => {
+        const getRandomImg = async () => {
+            try {
+                const firstWord = fact.split(' ')[0];
+                const response = await fetch(`https://cataas.com/cat/says/${firstWord}?json=true`);
+                const json = await response.json();
+                const { url } = json;
+                setImgUrl(url);
+            } catch (error) {
+                
+            }
+        };
+
+        getRandomImg();
+    }, [fact]);
+
     return (
         <main>
             <h1>App de gatitos</h1>
-            <p>{fact}</p>
+            <section>
+                <p>{fact}</p>
+                <img 
+                    src={`${CAT_ENDPOINT}${imgUrl}`} 
+                    alt={`Imagén obtenida de ${imgUrl} por hecho aleatorio`} 
+                />
+            </section>
         </main>
     );
 }
