7cd8d1fc21adbbc130543808c4305e4e4a5726e0
rolando22 <rolando2207@gmail.com>
2023-06-17 20:49:34 -0300

    [ADD] estilos

--- main.jsx
+++ main.jsx
@@ -2,5 +2,7 @@ import { createRoot } from 'react-dom/client';
 
 import { App } from './src/App';
 
+import './index.css';
+
 const root = createRoot(document.getElementById('app'));
 root.render(<App />);
